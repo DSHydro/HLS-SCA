ca198794dca6099483bdfff819c5978b88fa4324
Charles Nguyen <charlesdnguyen03@gmail.com>
2024-05-10 00:37:33 -0700

    More organization, + extracted data names into data_names.py

---
+++ Code/data_names.py
@@ -0,0 +1,5 @@
+# region = "HLS.S30.T10TEL.2023042T190521.v2.0"
+# lidar_name = "ASO_SacramentoMcCloud_2023Feb09-11_snowdepth_50m"
+
+region = "HLS.S30.T10TEL.2023137T185919.v2.0"
+lidar_name = "ASO_SacramentoMcCloud_2023May15-16_snowdepth_50m"
